6520775c9599619b36f8ae8a04b017f168b22bda
Administrator <admin@example.com>
2020-04-08 19:10:38 +0000

    Update test/test.sh

--- test/test.sh
+++ test/test.sh
@@ -15,23 +15,23 @@ fi
 echo $SCRIPT_DIR
 echo $CODE_DIR
 
-if [[ -z $($DOCKER_CMD images | grep test-container) ]] ; then
-    echo "Building test container"
-    docker build -t test-container $SCRIPT_DIR > /dev/null
-fi
+# if [[ -z $($DOCKER_CMD images | grep test-container) ]] ; then
+#     echo "Building test container"
+#     docker build -t test-container $SCRIPT_DIR > /dev/null
+# fi
 
-echo "Testing $1"
-CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
-GOPATH=${PWD}/vendor
-$DOCKER_CMD run \
-    --rm \
-    --name test \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v $CODE_DIR:$CODE_DIR -w $CODE_DIR \
-    -e TRAVIS_JOB_ID=$TRAVIS_JOB_ID \
-    -e TRAVIS_BRANCH=$TRAVIS_BRANCH \
-    -e TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST \
-    -e TRAVIS=$TRAVIS \
-    -e GOPATH=$GOPATH \
-    test-container \
-    sh -c "export PYTHONPATH=\$PYTHONPATH:\$PWD/test ; python test/$@"
+# echo "Testing $1"
+# CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
+# GOPATH=${PWD}/vendor
+# $DOCKER_CMD run \
+#     --rm \
+#     --name test \
+#     -v /var/run/docker.sock:/var/run/docker.sock \
+#     -v $CODE_DIR:$CODE_DIR -w $CODE_DIR \
+#     -e TRAVIS_JOB_ID=$TRAVIS_JOB_ID \
+#     -e TRAVIS_BRANCH=$TRAVIS_BRANCH \
+#     -e TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST \
+#     -e TRAVIS=$TRAVIS \
+#     -e GOPATH=$GOPATH \
+#     test-container \
+#     sh -c "export PYTHONPATH=\$PYTHONPATH:\$PWD/test ; python test/$@"
