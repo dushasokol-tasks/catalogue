85d8a5dfdeb994de56be4d5025ab557144c702eb
Administrator <admin@example.com>
2020-04-09 16:34:57 +0000

    Update test/test1.sh, .gitlab-ci.yml files

--- test/test1.sh
+++ test/test1.sh
@@ -52,21 +52,3 @@ $DOCKER_CMD run \
     -e GOPATH=$GOPATH \
     test-container \
     sh -c "export PYTHONPATH=\$PYTHONPATH:\$PWD/test ; python test/$@"
-
-# $DOCKER_CMD run \
-#     --tty \
-#     --rm \
-#     --name test \
-#     -v /var/run/docker.sock:/var/run/docker.sock \
-#     -v $SHARED_PATH:$SHARED_PATH -w $SHARED_PATH \
-#     -e TRAVIS_JOB_ID=$TRAVIS_JOB_ID \
-#     -e TRAVIS_BRANCH=$TRAVIS_BRANCH \
-#     -e TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST \
-#     -e TRAVIS=$TRAVIS \
-#     -e GOPATH=$GOPATH \
-#     test-container \
-#     sh -c "export PYTHONPATH=\$PYTHONPATH:\$SHARED_PATH/test ; ls -a ; python test/$@"
-
-
-#sh -c "export PYTHONPATH=\$PYTHONPATH:\$PWD/test ; ls;" export PYTHONPATH=\$PATH:/usr/local/bin/python     -e PYTHONPATH=$PYTHONPATH \
-    #sh -c "export PYTHONPATH=\$PYTHONPATH:\$PWD/test ; ls; pwd; python test/$@"
