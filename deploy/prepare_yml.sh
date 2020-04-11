81217c4a1b8e6ada3655fa1584d74dc615dd3811
Administrator <admin@example.com>
2020-04-11 09:30:20 +0000

    Update deploy/prepare_yml.sh

--- deploy/prepare_yml.sh
+++ deploy/prepare_yml.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-STAGE_CLUSTER_EXISTS = ''
+STAGE_CLUSTER_EXISTS=''
 
      echo "$SERVICE_ACCOUNT_KEY" > key.json
      gcloud auth activate-service-account --key-file=key.json
@@ -10,11 +10,11 @@ STAGE_CLUSTER_EXISTS = ''
 
      #STAGE_CLUSTER_NAME = $(gcloud container clusters list | grep -c "${CLUSTER_NAME}")
      echo "CN  ${CLUSTER_NAME}"
-STAGE_CLUSTER_EXISTS = $(gcloud container clusters list | grep -c ${CLUSTER_NAME})
+STAGE_CLUSTER_EXISTS=$(gcloud container clusters list | grep -c ${CLUSTER_NAME})
 
      echo " STTTTT $STAGE_CLUSTER_EXISTS"
 
-     if [[ $STAGE_CLUSTER_EXISTS != 1 ]]; then
+     if [[ $STAGE_CLUSTER_EXISTS!=1 ]]; then
         gcloud container clusters create $CLUSTER_NAME --enable-autoupgrade --enable-autoscaling --min-nodes=2 --max-nodes=4 --num-nodes=4 --zone=$REGION-$ZONE_EXTENSION
         kubectl create namespace sock-shop
      fi
