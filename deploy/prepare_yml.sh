7bf7581f62d19272067bc3bd9bf8a4f7b393f6a3
Administrator <admin@example.com>
2020-04-11 09:36:46 +0000

    Update deploy/prepare_yml.sh

--- deploy/prepare_yml.sh
+++ deploy/prepare_yml.sh
@@ -19,5 +19,6 @@ STAGE_CLUSTER_EXISTS=$(gcloud container clusters list | grep -c ${CLUSTER_NAME})
         gcloud container clusters create $CLUSTER_NAME --enable-autoupgrade --enable-autoscaling --min-nodes=2 --max-nodes=4 --num-nodes=4 --zone=$REGION-$ZONE_EXTENSION
         kubectl create namespace sock-shop
      fi
-             kubectl create namespace sock-shop
+
      gcloud container clusters get-credentials $CLUSTER_NAME --zone $REGION-$ZONE_EXTENSION
+                  kubectl create namespace sock-shop
