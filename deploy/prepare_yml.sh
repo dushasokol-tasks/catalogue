7b3f6ea45740bb4a5b4157b29a5654c6661d5cae
Administrator <admin@example.com>
2020-04-11 09:05:57 +0000

    Update .gitlab-ci.yml, deploy/prepare_yml.sh files

--- deploy/prepare_yml.sh
+++ deploy/prepare_yml.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
-ls -al
-source deploy/stage.yml
-cat > deploy.yml <<-EOF
+     echo "$SERVICE_ACCOUNT_KEY" > key.json
+     gcloud auth activate-service-account --key-file=key.json
+     gcloud config set project $PROJECT
+     gcloud config set container/cluster $CLUSTER_NAME
+     gcloud config set compute/zone $REGION-$ZONE_EXTENSION
 
-EOF
+     stage_cluster_name = result=$(gcloud container clusters list | grep -c "${CLUSTER_NAME}")
+
+
+     echo stage_cluster_name
+
+     if [[ $stage_cluster_name != $CLUSTER_NAME ]]; then
+        gcloud container clusters create $CLUSTER_NAME --enable-autoupgrade --enable-autoscaling --min-nodes=2 --max-nodes=4 --num-nodes=4 --zone=$REGION-$ZONE_EXTENSION
+        kubectl create namespace sock-shop
+     fi
+     
+     gcloud container clusters get-credentials $CLUSTER_NAME --zone $REGION-$ZONE_EXTENSION
